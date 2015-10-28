3dd3f6c9dc7b77fc90cf416edf33e38ab1a0e3b8
Otto Rask <ojrask@gmail.com>
2015-10-28 23:05:39 +0200

    Adjust Travis configuration, implement Travis bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,29 +6,47 @@ if (php_sapi_name() !== 'cli') {
     exit(1);
 }
 
-$pathParts = explode('wp-content', __DIR__);
 
-$wpRoot = array_shift($pathParts);
+if (strpos(__DIR__, 'wp-content') !== false) {
+    $pathParts = explode('wp-content', __DIR__);
 
-define('SHORT_INIT', true);
-define('WP_MEMORY_LIMIT', '256M');
+    $wpRoot = array_shift($pathParts);
+    define('WPRSRV_TRAVIS', false);
+} else {
+    $wpRoot = '/tmp/wordpress-tests-lib/';
+    define('WPRSRV_TRAVIS', true);
+}
 
-require_once($wpRoot . 'wp-load.php');
-require_once($wpRoot . 'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'admin.php');
+function loadPlugin()
+{
+    require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'wprsrv.php');
+}
 
-global $wpdb;
+if (WPRSRV_TRAVIS) {
+    require_once $wpRoot . 'includes/functions.php';
+
+    tests_add_filter('muplugins_loaded', 'loadPlugin');
+
+    require $wpRoot . 'includes/bootstrap.php';
+} else {
+    define('SHORT_INIT', true);
+    define('WP_MEMORY_LIMIT', '256M');
+
+    require_once($wpRoot . 'wp-load.php');
+    require_once($wpRoot . 'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'admin.php');
+
+    global $wpdb;
 
 // Begin MySQL transactions so we don't mod the database records.
-$wpdb->query('SET autocommit=0');
-$wpdb->query('START TRANSACTION');
+    $wpdb->query('SET autocommit=0');
+    $wpdb->query('START TRANSACTION');
 
-add_action('muplugins_loaded', function () {
-    include_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'wprsrv.php');
-});
+    add_action('muplugins_loaded', 'loadPlugin');
 
 // Rollback database transaction on script end.
-register_shutdown_function(function () use ($wpdb) {
-    $wpdb->query('ROLLBACK');
-});
+    register_shutdown_function(function () use ($wpdb) {
+        $wpdb->query('ROLLBACK');
+    });
 
-wp_set_current_user(1);
+    wp_set_current_user(1);
+}
