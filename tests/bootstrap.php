1d054c662f942084a3c91eebb33a1eb807e7c12a
Otto Rask <ojrask@gmail.com>
2015-10-28 22:43:03 +0200

    Add Travis config, additional test for Reservable creation
    The Travis configuration is copied from a few sources online.
    
    Additionally there is a new test for creating a reservable.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,6 @@
 
 namespace Wprsrv\Tests;
 
-use Wprsrv\PostTypes\Objects\Reservable;
-use Wprsrv\PostTypes\Objects\Reservation;
-
 if (php_sapi_name() !== 'cli') {
     exit(1);
 }
@@ -25,17 +22,8 @@ global $wpdb;
 $wpdb->query('SET autocommit=0');
 $wpdb->query('START TRANSACTION');
 
-/*
-// Remove all posts relating to plugin.
-$resIds = $wpdb->get_col(sprintf('SELECT ID FROM %s WHERE post_type in ("reservable", "reservation")', $wpdb->posts));
-$resIdsString = implode(',', $resIds);
-
-$wpdb->query(sprintf('DELETE FROM %s WHERE post_id IN (%s)', $wpdb->postmeta, $resIdsString));
-$wpdb->query(sprintf('DELETE FROM %s WHERE ID IN (%s)', $wpdb->posts, $resIdsString));
-*/
-
 add_action('muplugins_loaded', function () {
-    activate_plugin('wprsrv/wprsrv.php');
+    include_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'wprsrv.php');
 });
 
 // Rollback database transaction on script end.
